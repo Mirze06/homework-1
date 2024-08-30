072d2a115da65d70ad9398cf5364839b195a2834
Mirze06 <guleliyevmirze@gmail.com>
2024-08-30 18:24:14 +0400

    first commit

---
+++ solved.py
@@ -0,0 +1,33 @@
+# 1 rod = 5.0292 meters
+ROD = 5.0292
+
+# 1 furlong = 40 rods
+FURLONG = 40
+
+# 1 mile = 1609.34 meters
+MILE = 1609.34
+
+# 1 foot = 0.3048 meters
+FOOT = 0.3048
+
+# average walking speed is 3.1 miles per hour
+WALKING_SPEED = 3.1 / 60
+
+rods = input("Input rods:")
+
+rods = float(rods)
+meters = ROD*rods
+feet = meters/FOOT
+miles = meters/MILE
+furlongs = rods/FURLONG
+minutes = meters /(3.1*1609.34/60)
+
+
+
+print("Your input:", rods, "rods.", "\n\n")
+print("Conversions")
+print("Meters:", meters)
+print("Feet:", feet)
+print("Miles:", miles)
+print("Furlongs:", furlongs)
+print("Minutes to walk", rods, "rods:", minutes)
